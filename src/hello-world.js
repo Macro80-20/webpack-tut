6ee850cb7360b814c8a7409465f6eb295f4da763
Macro80-20 <fernando_lopezc@live.com>
2020-01-02 16:00:26 +0000

    Refactor to generate multiple html files

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,12 +1,8 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
-import addImage from "./add-image.js";
 import Heading from "./components/heading/heading";
 // helloWorldButton();
-addImage();
 const heading = new Heading();
 heading.render();
-const heading2 = new Heading();
-heading2.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,8 +2,7 @@ const path = require("path");
 // we usually dont need webpack plugs in production servers so we always sace as dev depend
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // installed via npm
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // minimal webpack config
 module.exports = {
   //entry points
@@ -16,7 +15,8 @@ module.exports = {
   output: {
     // add md5 hash to the contents of the file
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "./dist"),
+    // since i have more than one entrypoint i nee to specify somehwere that their names should be different
+    //webpack will takw the nbme of the property from the entry point configuration and put it in the filename!
     //  webpack where all generated files are located
     publicPath: ""
     // to show the a page from website you would use http
@@ -71,29 +71,40 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      //we can extract our css into a separate file and even specify the name
-      filename: "[name]-styles.[contenthash].css"
+      //we can extract our css into a separate file and even specify the name. in this case i used name so the bundle uses
+      // filename: "bundle.[contenthash].css"
+      filename: "[name].[contenthash].css"
     }),
     //everytime we run webpack this plugin removes all the files from the output folder essentially cleaning it
     new CleanWebpackPlugin({
       // i can specify an array of the file pattterns which i want to remove. all patterns are relative to the webpack out.path directory
       // in this case the build/subgfolder
-      cleanOnceBeforeBuildPatternsPatterns: [
-        "**/*", // this is how build paterns look like and it merans clean everything
-        path.join(process.cwd(), "build/**/*")
-      ]
+      // cleanOnceBeforeBuildPatternsPatterns: [
+      //   "**/*", // this is how build paterns look like and it merans clean everything. i rmeoved the folders with build
+      //   path.join(process.cwd(), "build/**/*")
+      // ]
     }),
     new HtmlWebpackPlugin({
       title: "Hello world",
       // this tells webpack to create a subfolder in the dist folder and put the html file inside this folder
-      // filename: "subfolder/custom_filename.html",
+      filename: "hello-world.html",
       // meta: {
       //   //this tells webpack to add a desciprtion meta tag to the page
       //   description: "Some description"
       // },
-      template: "src/index.hbs",
+      template: "src/page-template.hbs",
       // we use description variable in our hbs template to cusomise the index.html file
-      description: "Some description"
+      description: "Some description",
+      //How do we know which byndles to includes in our html? chunk allows us to do this and must be same name as the js bundle
+      chunks: ["hello-world-page"]
+    }),
+    // need to include two htmplugins if we want to begin to begin bundling for MPA's
+    new HtmlWebpackPlugin({
+      title: "Smiley Face",
+      filename: "smiley-face.html",
+      template: "src/page-template.hbs",
+      description: "smiley face",
+      chunks: ["smiley-page"]
     })
   ]
 };
